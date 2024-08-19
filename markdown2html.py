c32f00425ec2fe275f278a14caacb37792d260ee
Samar <bantmasr21@gmail.com>
2024-08-19 22:09:43 +0300

    added parsing paragraph

--- markdown2html.py
+++ markdown2html.py
@@ -25,6 +25,8 @@ try:
         html_content = ""
         in_unordered_list = False # flag to check if we are inside an unordered list
         in_ordered_list = False # flag to check if we are inside an ordered list
+        in_paragraph = False # flag to check if we are inside a paragraph
+        current_paragraph = ""
 
         for line in lines:
             stripped_line = line.strip()
@@ -37,7 +39,7 @@ try:
                     heading_text = stripped_line[heading_level:].strip()
                     html_content += f"<h{heading_level}>{heading_text}</h{heading_level}>\n"
 
-                # close any open list before processing a heading
+                # close any open list or paragraph before processing a heading
                 if in_unordered_list:
                     html_content += "</ul>\n"
                     in_unordered_list = False
@@ -46,12 +48,23 @@ try:
                     html_content += "</ol>\n"
                     in_ordered_list = False
 
+                if in_paragraph:
+                    lines_in_paragraph = "\n\t<br />\n\t".join(current_paragraph.split("\n"))
+                    html_content += f"<p>\n\t{lines_in_paragraph}\n</p>\n"
+                    in_paragraph = False
+                    current_paragraph = ""
+
             #check for unordered list items
             elif stripped_line.startswith("- "):
                 if not in_unordered_list:
                     if in_ordered_list: # close ordered list if open
                         html_content += "</ol>\n"
                         in_ordered_list = False
+                    if in_paragraph: # closr paragraph if open
+                        lines_in_paragraph = "\n\t<br />\n\t".join(current_paragraph.split("\n"))
+                        html_content += f"<p>\n\t{lines_in_paragraph}\n</p>\n"
+                        in_paragraph = False
+                        current_paragraph = ""
                     html_content += "<ul>\n"
                     in_unordered_list = True
                 list_item = stripped_line[2:].strip()
@@ -63,26 +76,48 @@ try:
                     if in_unordered_list: # close unordered list if open
                         html_content += "</ul>\n"
                         in_unordered_list = False
+                    if in_paragraph: # close paragraph
+                        lines_in_paragraph = "\n\t<br />\n\t".join(current_paragraph.split("\n"))
+                        html_content += f"<p>\n\t{lines_in_paragraph}\n</p>\n"
+                        in_paragraph = False
+                        current_paragraph = ""
                     html_content += "<ol>\n"
                     in_ordered_list = True
                 list_item = stripped_line[2:].strip()
                 html_content += f"  <li>{list_item}</li>\n"
 
+            # check for paragraph text
             else:
-                # if we encounter a non-list and non-heading line, we are in a list, close the list
-                if in_unordered_list:
-                    html_content += "</ul>\n"
-                    in_unordered_list = False
-                if in_ordered_list:
-                    html_content += "</ol>\n"
-                    in_ordered_list = False
+                if stripped_line: # non-empty line
+                    if not in_paragraph:
+                    # if we encounter a non-list and non-heading line, we are in a list, close the list
+                        if in_unordered_list:
+                            html_content += "</ul>\n"
+                            in_unordered_list = False
+                        if in_ordered_list:
+                            html_content += "</ol>\n"
+                            in_ordered_list = False
+                        in_paragraph = True
+                        current_paragraph = stripped_line
+                    else:
+                        current_paragraph += f"\n{stripped_line}"
+                else: # empty line indicates the end of a paragraph
+                    if in_paragraph:
+                        # split the paragraph text into lines and indent each line
+                        lines_in_paragraph = "\n\t<br />\n\t".join(current_paragraph.split("\n"))
+                        html_content += f"<p>\n\t{lines_in_paragraph}\n</p>\n"
+                        in_paragraph = False
+                        current_paragraph = "" 
 
-        # close any remaining open lists at the end of the file
+        # close any remaining open lists or paragraphs at the end of the file
         if in_unordered_list:
             html_content += "</ul>\n"
         if in_ordered_list:
             html_content += "</ol>\n"
-            
+        if in_paragraph:
+            lines_in_paragraph = "\n\t<br />\n\t".join(current_paragraph.split("\n"))
+            html_content += f"<p>\n\t{lines_in_paragraph}\n</p>\n"
+
         # write the html content to the html_file
         with open(html_file, 'w') as html:
             html.write(html_content)
