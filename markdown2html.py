c0f020866666e01e0a460016dea069bd772d59f0
Samar <bantmasr21@gmail.com>
2024-08-23 04:31:49 +0300

    fixed

--- markdown2html.py
+++ markdown2html.py
@@ -7,11 +7,13 @@ import os
 
 
 if len(sys.argv) < 3:
+    """nn"""
     sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
     sys.exit(1)
 
 input_file = sys.argv[1]
 if not os.path.exists(input_file):
+    """mmm"""
     sys.stderr.write(f"Missing {input_file}\n")
     sys.exit(1)
 
